aa8bbe843fe674f8e1a48c5017023d7ac45081f0
Katie Bosman <katherinebosman@gmail.com>
2024-02-14 13:08:38 -0400

    Add metadata correction, handling of subfolder/channel input lists, various other points

--- miniseed_recut.py
+++ miniseed_recut.py
@@ -7,8 +7,10 @@ June 6, 2023
 import argparse
 from datetime import datetime, timedelta
 from glob import glob
+import numpy as np
 import obspy
 import os
+import pandas as pd
 import traceback
 
 import nfsi_obs as nf
@@ -17,26 +19,55 @@ from utilities import logger
 # TODO: Make script callable with arguments (easier to reuse)
 # TODO: Include metadata correction (trace IDs)
 
-channels_of_interest = ['SeisE', 'SeisN', 'SeisZ', 'SeisX']
-base_dir = 'L:/Data/Ischia test deployment'
-#subfolders = ['AQU-4261', 'AQU-8263', 'AQU-B063', 'AQU-8063-fixed']
-subfolders = ['AQU-8063-fixed']
+# Default SDS archive folder.
+DEFAULT_ARCHIVE = 'L:/Data/SDS'
+DEFAULT_CHANNELS = ['S1SeisEFR', 'S1SeisNFR', 'S1SeisZFR', 'S1SeisXFR']
 
-#outdir = os.path.join(base_dir, 'Day-long mseed')
-# Default SDS archive folder. TODO: Make CL option.
-arcdir = 'L:/Data/SDS'
-if not os.path.exists(arcdir):
-    os.makedirs(arcdir)
+def make_daily_miniseed_files(data_dir, archive_dir, subfolders=None, channels=None, correct_meta=False, metadata_args=None):
+    if channels is None:
+        channels = DEFAULT_CHANNELS
 
-for sf in subfolders:
-    print('Checking folder {}...'.format(sf))
-    for ci in channels_of_interest:
-        print('Looking for channel {}...'.format(ci))
-        data_files = glob(os.path.join(base_dir, sf, '**', '*'+ci+'*.mseed'), recursive=True)
+    # Get full list of files to process
+    data_files = []
+    if subfolders is None:
+        if channels == 'all':
+            data_files = glob(os.path.join(data_dir, '**', '*.mseed'), recursive=True)
+        elif isinstance(channels, list):
+            for ch in channels:
+                temp_files = glob(os.path.join(data_dir, '**', '*'+ch+'*.mseed'), recursive=True)
+                data_files.extend(temp_files)
+        else:
+            raise TypeError('Unrecognized value of input `channels`: {}'.format(channels))
+    elif isinstance(subfolders, list):
+        for sf in subfolders:
+            if channels == 'all':
+                temp_files = glob(os.path.join(data_dir, sf, '**', '*.mseed'), recursive=True)
+                data_files.extend(temp_files)
+            elif isinstance(channels, list):
+                for ch in channels:
+                    temp_files = glob(os.path.join(data_dir, sf, '**', '*'+ch+'*.mseed'), recursive=True)
+                    data_files.extend(temp_files)
+            else:
+                raise TypeError('Unrecognized value of input `channels`: {}'.format(channels))
+    else:
+        raise TypeError('Unrecognized value of input `subfolders`: {}'.format(subfolders))
+
+    # Label data files by channel name
+    labels = []
+    for df in data_files:
+        file_name = os.path.basename(df)
+        ch_name = file_name.split('_')[1]
+        labels.append({'channel': ch_name, 'path': df})
+    labeled_files = pd.DataFrame(labels)
+    g_log.info("Files contain data for {0} unique set(s) of channels".format(len(np.unique(labeled_files['channel'].values))))
+
+    # Process data files by channel
+    for label, files in labeled_files.groupby('channel'):
+        g_log.info('Processing channel {}...'.format(label))
 
         full_data = obspy.Stream()
-        for df in data_files:
-            print('Reading {}...'.format(df))
+        for df in files:
+            g_log.info('Reading {}...'.format(df))
             try:
                 temp = obspy.read(df, header_byteorder='>')
                 for tr in temp:
@@ -47,9 +78,37 @@ for sf in subfolders:
                 continue
 
         full_data.merge()
-        print(full_data)
+        g_log.info(full_data)
         full_data.print_gaps()
 
+        # Correct metadata (if applicable)
+        if correct_meta:
+            if not isinstance(metadata_args, dict):
+                raise TypeError('Unrecognized type for metadata arguments (should be dict): {}'.format(type(metadata_args)))
+
+            station_info, channel_map = None, None
+            if 'metadata_file' in metadata_args:
+                g_log.info("Reading metadata from file {0}".format(metadata_args['metadata_file']))
+                filetype = os.path.splitext(metadata_args['metadata_file'])[-1]
+                if filetype in ['.dataless', '.metadata']:
+                    station_info = nf.metadata.read_dataless(metadata_args['metadata_file'])
+                elif filetype == '.xml':
+                    # read as StationXML format
+                    station_info = obspy.read_inventory(metadata_args['metadata_file'])
+                else:
+                    g_log.error("Unrecognized file format. Unable to read metadata.")
+
+            if 'channel_map' in metadata_args:
+                g_log.info("Reading channel ID mapping from file {0}".format(metadata_args['channel_map']))
+                channel_map = nf.io.read_channel_map(metadata_args['channel_map'])
+
+            net_id = 'XX'
+            if 'network_id' in metadata_args:
+                net_id = metadata_args['network_id']
+
+            full_data = nf.metadata.update_metadata(full_data, net_id, g_log, station_info, channel_map)
+
+        # Cut and save day-long miniSEED files in SDS archive structure
         for tr in full_data:
             start = tr.stats.starttime.datetime
             end = tr.stats.endtime.datetime + timedelta(days=1)
@@ -60,9 +119,9 @@ for sf in subfolders:
             while cut < endday:
                 temp = tr.slice(cut, cut + 24 * 60 * 60, nearest_sample=False)
                 stt = temp.split()  # deal with traces with gaps
-                print(stt)
+                g_log.info(stt)
 
-                output_dir = os.path.join(arcdir, str(cut.year), tr.stats.network, tr.stats.station, tr.stats.channel)
+                output_dir = os.path.join(archive_dir, str(cut.year), tr.stats.network, tr.stats.station, tr.stats.channel)
                 if not os.path.exists(output_dir):
                     os.makedirs(output_dir)
 
@@ -87,7 +146,9 @@ if __name__ == '__main__':
                         help="Network identifier assigned by FDSN for this project. Default 'XX' for test data.")
     parser.add_argument('--channelmap', dest="channel_map",
                         help="File mapping as-recorded channel codes to their correct values.")
-    parser.add_argument('--metadata', dest="metadata_file", help="Path to metadata file (dataless SEED or StationXML)")
+    parser.add_argument('--metadata', dest="metadata_file",
+                        help="Path to metadata file (dataless SEED or StationXML). Channel IDs should match the raw "
+                             "data (not corrected by channel_map).")
 
     try:
         args = parser.parse_args()
@@ -103,10 +164,17 @@ if __name__ == '__main__':
         else:
             raise SyntaxError('No input data directory specified!')
 
-        output_dir = None
-        if args.outdir is not None:
-            output_dir = os.path.abspath(os.path.expanduser(os.path.expandvars(args.outdir)))
+        arc_dir = DEFAULT_ARCHIVE
+        if args.arc_dir:
+            arc_dir = os.path.abspath(os.path.expanduser(os.path.expandvars(args.arc_dir)))
 
+        subfolders, channels = None, None
+        if args.subfolders:
+            subfolders = args.subfolders.split(',')
+        if args.channels:
+            channels = args.channels.split(',')
+
+        meta_args = None
         if args.correct_metadata:
             channel_map = None
             if args.channel_map:
@@ -116,10 +184,15 @@ if __name__ == '__main__':
             if args.metadata_file:
                 metadata_file = os.path.abspath(os.path.expanduser(os.path.expandvars(args.metadata_file)))
 
-            # Process data files to apply clock drift correction and update metadata
-            fix_metadata(data_dir, row, args.network_id, output_dir, channel_map, metadata_file)
+            meta_args = {
+                'channel_map': channel_map,
+                'metadata_file': metadata_file,
+                'network': args.network_id
+            }
 
         # Split data into day-long miniSEED files saved in archive_dir (SDS folder structure)
+        make_daily_miniseed_files(data_dir, arc_dir, subfolders=subfolders, channels=channels,
+                                  correct_meta=args.correct_metadata, metadata_args=meta_args)
 
         g_log.info("Processing complete!")
         logger.close_logs()
