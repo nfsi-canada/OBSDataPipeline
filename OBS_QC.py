c55d50dcafceb8f8cc386483071268d02271e37c
Katie Bosman <katherinebosman@gmail.com>
2025-02-21 14:09:35 -0400

    Debug time at seafloor not displaying in report summary

--- OBS_QC.py
+++ OBS_QC.py
@@ -108,7 +108,8 @@ def process(data_dir, obs_log, network_id, config, output_dir=None, metadata=Non
             data_start.strftime('%Y-%m-%d %H:%M:%S'), data_end.strftime('%Y-%m-%d %H:%M:%S')))
         data_start, data_end = None, None
 
-    report_kwargs['seafloorDays'] = '{:.3f}'.format((data_end - data_start) / 60 / 60 / 24)
+    report_params['seafloorDays'] = '{:.3f}'.format((data_end - data_start) / 60 / 60 / 24)
+    g_log.debug('Time at seafloor: {} days'.format(report_params['seafloorDays']))
 
     # Read station metadata file (dataless SEED or StationXML)
     station_info = None
