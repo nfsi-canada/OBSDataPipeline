f717e8c79a6aec409d3745984b36a579b99bc441
Katie Bosman <katherinebosman@gmail.com>
2025-05-27 14:17:54 -0300

    Add start/end dates to station object in StationXML file. Updated TODO for full-network StationXML compilation to include start/end time of network.

--- edit_stationxml.py
+++ edit_stationxml.py
@@ -196,6 +196,8 @@ def update_station_xml(inv, obs_log=None, extra_info=None, nfsi_fields=False, su
             s.longitude.__setattr__('measurement_method', survey_method)
             s.elevation.__setattr__('measurement_method', survey_method)
             s.water_level = 0
+            s.start_date = start
+            s.end_date = end
 
             sta_info = None
             if net_info is not None:
@@ -414,6 +416,7 @@ if __name__ == '__main__':
 
         g_log.info("Processing complete!")
         # TODO: Combine individual stations into full-network StationXML file
+        # TODO: Full-network StationXML should have start/end dates for network equal to earliest start and latest end for any station
         logger.close_logs()
     except Exception as e:
         print(traceback.print_exc())
